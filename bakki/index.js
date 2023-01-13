b7fe05cd3730cbac5b176298a39f28b55bdc0dcd
Jaakkosa <jaakko.satuli@gmail.com>
2023-01-13 12:48:27 +0200

    push

--- bakki/index.js
+++ bakki/index.js
@@ -6,10 +6,7 @@ app.use(express.static('build'))
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
-/*
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-}) */
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- frontti/src/App.js
+++ frontti/src/App.js
@@ -2,10 +2,52 @@ import './index.css'
 import { Dots } from './components/dots';
 import { FirstPage } from './components/firstPage';
 import { Info } from './components/selitys';
+import { useEffect, useRef } from 'react';
+import { animate } from 'framer-motion';
 
 
 const App = () => {
-document.title = "JaakkoSatuli"
+  
+  const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".ympyra");
+
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+ 
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles(); 
+
 
 
   return(
@@ -14,6 +56,7 @@ document.title = "JaakkoSatuli"
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <FirstPage/>
     <Info elementti={document.getElementById('Info')}/>
+ 
  </div>
  </>
   ) 
